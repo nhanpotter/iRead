0d4a388bbcdf0515279e2797af4c4b33acb29129
Duc Ta <anhduc001@e.ntu.edu.sg>
2020-03-21 05:06:06 +0800

    Commit

---
+++ app/src/main/java/com/example/iread/ui/activities/HomePage/bk3.java
@@ -0,0 +1,16 @@
+package com.example.iread.ui.activities.HomePage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.iread.R;
+
+public class bk3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bk3);
+    }
+}
