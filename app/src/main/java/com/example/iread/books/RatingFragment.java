942e0bbc1599ccdf528b0bd019a6f217d83518b8
Duc Ta <anhduc001@e.ntu.edu.sg>
2020-04-01 14:55:24 +0800

    Create RatingFragment, bind thumbnails and covers to books

--- app/src/main/java/com/example/iread/books/RatingFragment.java
+++ app/src/main/java/com/example/iread/books/RatingFragment.java
@@ -7,35 +7,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.navigation.Navigation;
 
 import com.example.iread.MyApplication;
 import com.example.iread.R;
 
-import java.util.List;
-
 import javax.inject.Inject;
 
-public class CommentFragment extends DialogFragment {
+public class RatingFragment extends DialogFragment {
     private View rootView;
 
-    private TextView commentField;
+    private RatingBar ratingBar;
 
     private int bookId;
+    private int userRating;
 
     @Inject
-    CommentViewModel commentViewModel;
+    RatingViewModel ratingViewModel;
 
-    public CommentFragment() {}
+    public RatingFragment() {}
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -49,22 +45,33 @@ public class CommentFragment extends DialogFragment {
 
         LayoutInflater inflater = requireActivity().getLayoutInflater();
 
-        View rootView = inflater.inflate(R.layout.fragment_addcomment, null);
-        commentField = rootView.findViewById(R.id.comment);
+        View rootView = inflater.inflate(R.layout.fragment_addrating, null);
+        ratingBar = rootView.findViewById(R.id.ratingBar2);
 
-        builder.setTitle("Comment")
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b){
+                userRating = (int) ratingBar.getRating();
+            }
+        });
+
+        builder.setTitle("Rating")
                 .setView(rootView)
-                .setPositiveButton(R.string.send_comment, new DialogInterface.OnClickListener() {
+                .setPositiveButton(R.string.send_rating, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        String comment = commentField.getText().toString();
-                        commentViewModel.postComment(bookId, comment);
+                        ratingViewModel.postRating(bookId, userRating);
+
+                        String message = "Thank you for rating the book!\n" +
+                                "Your rating is " + userRating;
+                        Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
                     }
                 })
                 .setNegativeButton(R.string.cancel_comment, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        CommentFragment.this.getDialog().cancel();
+                        RatingFragment.this.getDialog().cancel();
                     }
                 });
 
@@ -74,7 +81,7 @@ public class CommentFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_addcomment, container, false);
+        rootView = inflater.inflate(R.layout.fragment_addrating, container, false);
         bookId = getArguments().getInt("id");
 
         return rootView;
